3920f6f104e6b143bf854d66f9c05791f92baf13
Colby Beach <38380295+clawplusstacker@users.noreply.github.com>
2021-12-23 15:27:09 -0500

    Made homepage more CC

--- Website/home.js
+++ Website/home.js
@@ -1,16 +1,29 @@
 
 function originalCSS(){
 
-  $(".bannerContainer").css("min-height", "90%")
+  $(".navBarLogoText").css("margin-left", "90%")
+  $(".navBarLogoText").css("margin-top", "20px")
+  $(".navBarOtherlinks").css("margin-top", "15px")
+  $(".navBarOtherlinks").css("margin-right", "10px")
+  $(".navBarContact").css("margin-right", "90px")
+  $(".navBarContact").css("margin-top", "15px")
+
   $("#bannerLeft").css("float", "left")
+  $("#bannerLeft").css("width", "50%")
+  $("#bannerLeftText").css("margin-left", "30%")
+  $("#bannerLeftText").css("font-size", "70px")
   $("#bannerRight").show();
 
   $(".ssPic").css("width", "300")
 
   $(".homeBody2Header").css("padding-top", "130px")
-  $("#demoDesc").css("width", "25%")
-  $("iframe").show()
+  $("#demoDesc").show()
+  $("iframe").css("height", "490px")
+  $("iframe").css("width", "820px")
+  $("iframe").css("bottom", "63%")
+  $("iframe").css("left", "40%")
 
+  $(".homeBody3").css("height", "650px")
   $(".halfLeft").css("float", "left")
   $(".halfLeft").css("width", "50%")
   $(".halfLeft").css("height", "80%")
@@ -23,18 +36,32 @@ function originalCSS(){
 
 function changeCSS(){
 
- 
-  $(".bannerContainer").css("min-height", "100%")
+  $(".navBarLogoText").css("margin-left", "0%")
+  $(".navBarLogoText").css("margin-top", "0px")
+  $(".navBarOtherlinks").css("margin-right", "0px")
+  $(".navBarOtherlinks").css("margin-top", "0px")
+  $(".navBarContact").css("margin-right", "0px")
+  $(".navBarContact").css("margin-top", "0px")
+
+
   $("#bannerLeft").css("float", "none")
+  $("#bannerLeft").css("width", "100%")
+  $("#bannerLeftText").css("margin-left", "0%")
+  $("#bannerLeftText").css("font-size", "50px")
   $("#bannerRight").hide();
 
-  $(".ssPic").css("width", "100px")
+  $(".ssPic").css("width", "150px")
 
   $(".homeBody2Header").css("padding-top", "30px")
-  $("#demoDesc").css("width", "50%")
-  $("iframe").hide()
+  $("#demoDesc").hide()
+  $("iframe").css("height", "300px")
+  $("iframe").css("width", "400px")
+  $("iframe").css("bottom", "0%")
+  $("iframe").css("left", "10%")
 
-  $(".homeBody1").css("height", "1300px")
+
+
+  $(".homeBody3").css("height", "1300px")
   $(".halfLeft").css("float", "none")
   $(".halfLeft").css("width", "100%")
   $(".halfLeft").css("height", "35%")
