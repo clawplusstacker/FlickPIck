3aaad23830f41d78a570ba317a925383cf5dc342
Colby Beach <38380295+clawplusstacker@users.noreply.github.com>
2022-01-30 23:31:00 -0500

    Finished MovieView concept

--- XCode Project/FlickPick : MovieTinder/movieTinder/MovieView.swift
+++ XCode Project/FlickPick : MovieTinder/movieTinder/MovieView.swift
@@ -14,16 +14,34 @@ private var userStore = UserStoreFunctions()
 
 
 struct MovieView: View {
+    
+    init() {
+      let coloredAppearance = UINavigationBarAppearance()
+      coloredAppearance.configureWithOpaqueBackground()
+        coloredAppearance.backgroundColor = .white
+      coloredAppearance.titleTextAttributes = [.foregroundColor: UIColor.systemPink]
+      coloredAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.systemPink]
+      
+      UINavigationBar.appearance().standardAppearance = coloredAppearance
+      UINavigationBar.appearance().compactAppearance = coloredAppearance
+      UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
+      
+      UINavigationBar.appearance().tintColor = .white
+    }
+    
+    @State var updater = ""
+    
     var body: some View{
         
+        NavigationView {
+        
         VStack{
-            
             ZStack{
                 
                 let url = URL(string: "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg")
                                 
                 let fade = Gradient(stops: [.init(color: .clear, location: 0),
-                                             .init(color: .black, location: 2)])
+                                            .init(color: .black, location: 1.3)])
                 
                 if #available(iOS 15.0, *) {
                     AsyncImage(url: url) { phase in
@@ -41,247 +59,108 @@ struct MovieView: View {
                     }
                 }
                 
-                
-                
-                Text("Spider-Man: No Way Home")
-                    .foregroundColor(.white)
-                    .bold()
-                    .font(.system(size: 30))
-                    .frame(width: 200)
-                
-                
-            }
+                VStack{
+                    HStack{
+                        VStack(alignment: .leading){
+                            Text("Spider-Man: No Way Home")
+                                .foregroundColor(.white)
+                                .bold()
+                                .font(.system(size: 30))
+                            Text("(2021)")
+                                .foregroundColor(.white)
+                                .font(.system(size: 20))
+                        }
+                        .frame(width: 200)
+                        .padding(.horizontal, 50)
+                        
+                        Button {
+                            Text("hi")
+                            //Show MovieDetailView
+                        } label: {
+                            Image(systemName: "info.circle.fill")
+                                .foregroundColor(.white)
+                                .font(.system(size: 30))
+                                .padding(.horizontal, 60)
+                        } //Button
+                      
+                    } //HStack
+                    .offset(y: 250)
+
+                    HStack{
+                        
+                        ZStack{
+    
+                            Button(action: {
+                            }) {
+                                Image(systemName: "xmark.circle")
+                                    .foregroundColor(.white)
+                                    .font(.system(size: 60))
+                                    .shadow(color: .blue, radius: 5, x: 0, y: 3)
+                                }
+
+                            //Main Button Pressed
+                            Button(action: {
+    
+                                userStore.addToMoviesDisliked(index: userStore.getFireStoreUserIndex(uid: (Auth.auth().currentUser?.uid) ?? ""), movie_id: "")
+    
+                                //currentMovie = getCurrentMovie(randomNum: randomNum)
+                                updater =  ""
+                                updater =  " "
+    
+    
+    
+                            }) {
+                                Image(systemName: "xmark.circle.fill")
+                                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                                    .font(.system(size: 60))
+                                    
+                            }
+                        }   //ZStack (Dislike Buttons)
+                        .padding(.horizontal, 10)
+                        .padding(.bottom, 20)
+    
+    
+                        ZStack{
+    
+                            Button(action: {}) {
+                                    Image(systemName: "heart.circle")
+                                        .foregroundColor(.white)
+                                        .font(.system(size: 60))
+                                        .shadow(color: .pink, radius: 5, x: 0, y: 3)
+                                    }
+    
+                            //Main Button Pressed
+                            Button(action: {
+    
+                                userStore.addToMoviesLiked(index: userStore.getFireStoreUserIndex(uid: (Auth.auth().currentUser?.uid) ?? ""), movie_id: "")
+    
+    
+                                //currentMovie = getCurrentMovie(randomNum: randomNum)
+                                updater =  ""
+                                updater =  " "
+    
+                            }) {
+                                Image(systemName: "heart.circle.fill")
+                                    .foregroundColor(.pink)
+                                    .font(.system(size: 60))
+                            }//Button
+                            
+                        }//ZStack (Heart Buttons)
+                        .padding(.horizontal, 10)
+                        .padding(.bottom, 20)
+                        
+                    } //HStack (Buttons)
+                    .offset(x: 0, y: 250)
+                }//VStack (With Text)
+            } //ZStack
             .frame(width: 400, height: 700)
             .cornerRadius(30)
-        }
-    }
-}
-
-
-//struct MovieView: View {
-//
-//
-//    @State var updater = ""
-//    @State var moviePoster = ""
-//
-//
-//
-//    var body: some View {
-//
-//
-//
-//        ZStack{
-//
-//                VStack{
-//
-//                    Button {
-//
-//                        updater = ""
-//
-//
-//
-//                    } label: {
-//                        let url = URL(string: "https://image.tmdb.org/t/p/w500/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg")
-//
-//
-//                        if #available(iOS 15.0, *) {
-//                            AsyncImage(url: url) { phase in
-//                                if let image = phase.image {
-//                                    image
-//                                        .resizable()
-//                                        .aspectRatio(contentMode: .fill)
-//                                        .frame(width: 600, height: 400)
-//                                        .clipped()
-//
-//                                } else if phase.error != nil {
-//                                    Text("Network Error!")
-//                                        .frame(width: 600, height: 400)
-//
-//                                } else {
-//                                    ProgressView()
-//                                        .frame(width: 600, height: 400)
-//                                }
-//                            }
-//                        } else {
-//
-//                        } //If statement for iOS 15
-//                    } //Button
-//
-//
-//                    ScrollView{
-//
-//                        VStack{
-//
-//
-//                            HStack{
-//
-//                                HStack{
-//
-//
-//                                    Text("Title")
-//                                        .font(.system(size: 30).bold())
-//                                        .foregroundColor(.pink)
-//
-//
-//                                    Text("Year")
-//                                        .font(.system(size: 17).bold())
-//                                        .foregroundColor(.secondary)
-//
-//                                    Text(updater)
-//
-//
-//                                }
-//                                .padding(.init(top: 20, leading: 0, bottom: 0, trailing: 0))
-//
-//                                Spacer()
-//
-//                            }
-//                            .padding(.horizontal, 100)
-//
-//
-//                            HStack{
-//
-//                                Image("imdb")
-//                                    .resizable()
-//                                    .scaledToFit()
-//                                    .frame(width: 20, height: 20)
-//
-//                                Text("IMDB Rating: " + "/10")
-//                                    .font(.subheadline)
-//                                    .foregroundColor(.primary)
-//
-//
-//                                Spacer()
-//
-//                            }
-//                            .padding()
-//                            .padding(.horizontal, 85)
-//
-//
-//                            Divider()
-//                                .padding()
-//
-//
-//                            HStack{
-//
-//                                Image(systemName: "text.bubble").foregroundColor(Color.black)
-//                                    .font(.system(size: 20))
-//
-//                                Text("Description:")
-//                                    .font(.headline)
-//
-//                                Spacer()
-//
-//                            }
-//                            .padding(.horizontal, 85)
-//                            .padding()
-//
-//                            Text("Desc")
-//                                .font(.subheadline)
-//                                .foregroundColor(.secondary)
-//                                .padding(.horizontal, 130)
-//
-//                            Text("\n \n \n \n \n \n \n")
-//
-//
-//                        }   //Vstack For text
-//
-//                    Spacer()    //Pushes Descp/Pic up
-//                } //Scroll View
-//
-//            }//VStack for Pics/Text
-//
-//
-//            VStack{
-//
-//                Spacer()
-//
-//                HStack{
-//
-//                    ZStack{
-//
-//                        Button(action: {
-//                        }) {
-//                                Image(systemName: "xmark.circle")
-//                                    .foregroundColor(.white)
-//                                    .font(.system(size: 70))
-//                                    .padding(.horizontal, 50)
-//                                    .padding(.bottom, 20)
-//                                    .shadow(color: .blue, radius: 5, x: 0, y: 3)
-//                                }
-//
-//
-//                        //Main Button Pressed
-//                        Button(action: {
-//
-//                            userStore.addToMoviesDisliked(index: userStore.getFireStoreUserIndex(uid: (Auth.auth().currentUser?.uid) ?? ""), movie_id: "")
-//
-//                            //currentMovie = getCurrentMovie(randomNum: randomNum)
-//                            updater =  ""
-//                            updater =  " "
-//
-//
-//
-//                        }) {
-//                                Image(systemName: "xmark.circle.fill")
-//                                    .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-//                                    .font(.system(size: 70))
-//                                    .padding(.horizontal, 50)
-//                                    .padding(.bottom, 20)
-//
-//                                }
-//
-//                    }   //ZStack
-//
-//
-//                    ZStack{
-//
-//                        Button(action: {}) {
-//                                Image(systemName: "heart.circle")
-//                                    .foregroundColor(.white)
-//                                    .font(.system(size: 70))
-//                                    .padding(.horizontal, 50)
-//                                    .padding(.bottom, 20)
-//                                    .shadow(color: .pink, radius: 5, x: 0, y: 3)
-//                                }
-//
-//                        //Main Button Pressed
-//                        Button(action: {
-//
-//                            userStore.addToMoviesLiked(index: userStore.getFireStoreUserIndex(uid: (Auth.auth().currentUser?.uid) ?? ""), movie_id: "")
-//
-//
-//                            //currentMovie = getCurrentMovie(randomNum: randomNum)
-//                            updater =  ""
-//                            updater =  " "
-//
-//                        }) {
-//                                Image(systemName: "heart.circle.fill")
-//                                    .foregroundColor(.pink)
-//                                    .font(.system(size: 70))
-//                                    .padding(.horizontal, 50)
-//                                    .padding(.bottom, 20)
-//
-//
-//                                }
-//                    }//ZStack
-//
-//                }//Hs stack
-//
-//            }//VStack for like buttons
-//
-//        } //ZStack
-//
-//        .background(Image("whitePinkGradient"))
-//
-//        .onAppear() {
-//            //self.movieList.fetchData()
-//        }
-//
-//
-//    } //Var body : some View
-//} //Struct
-
+        } //VStack
+        .navigationTitle("FlickPick")
+        .navigationBarTitleDisplayMode(.inline)
+        }//Navigation view
+    } //Var body: some
+} //Struct
 
 
 
